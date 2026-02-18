d9533523ab8851ae1718ff1d24932de4ccb655bd
Ariel shachrur <ariel.shachrur@gmail.com>
2026-02-18 21:49:09 +0200

    with signalStorage.js

---
+++ frontend/src/services/signalStorage.js
@@ -0,0 +1,31 @@
+import util from 'tweetnacl-util';
+
+
+
+export function savePrivateKey(keyId, privateKeyUint8) //keyID - e.g IK or SPK or the specific ID of OPK
+{
+    const base64Key = util.encodeBase64(privateKeyUint8);
+    localStorage.setItem(`privateKey_${keyId}`, base64Key);
+}
+
+export function getPrivateKey(keyId) //keyID - e.g IK or SPK or the specific ID of OPK
+{
+    const base64Key = localStorage.getItem(`privateKey_${keyId}`);
+    if (!base64Key) {
+        return null;
+    }
+    return util.decodeBase64(base64Key);
+}
+
+export function saveRootKey(sessionId, rootKeyUint8) {
+    const base64Key = util.encodeBase64(rootKeyUint8);
+    localStorage.setItem(`rootKey_${sessionId}`, base64Key);
+}
+
+export function getRootKey(sessionId) {
+    const base64Key = localStorage.getItem(`rootKey_${sessionId}`);
+    if (!base64Key) {
+        return null;
+    }
+    return util.decodeBase64(base64Key);
+}
